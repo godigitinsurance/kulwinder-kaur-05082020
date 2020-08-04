e15d0416496f9960f62b8b174ed21623ff6c0336
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-08-04 19:54:50 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Kulwinder Kaur");
+$("#policyNumber").text("TO77320800");
+$("#startDate").text("05-Aug-2020");
+$("#endDate").text("04-Aug-2021");
+
+
+$("#type").text("Four Wheeler");
+$("#registrationNumber").text("PB08CW6207");
+$("#make").text("RENAULT");
+$("#model").text("PULSE");
+$("#vehicalSubType").text("RXL (P)");
+$("#engineNo").text("750707B");
+$("#chassis").text("MEEAHBA70E2500157");
